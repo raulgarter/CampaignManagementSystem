66700b2294a8d9a52ae5e5fd7d3058c63373bb95
ntjk <nattlyjk@gmail.com>
2020-10-04 23:24:01 -0400

    CSV, Español y cambio a interfaz de campaign.
    Botón CSV
    Español al Datatable
    Primer cambio de interfaz a campaign y se ve en el archivo llamado campaign2.html

---
+++ dist/js/csv-button.js
@@ -0,0 +1,33 @@
+$(function () {
+  var table = $('#example2').DataTable({
+    "oLanguage": {
+        "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrar _MENU_ registros",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+          "sFirst":    "Primero",
+          "sLast":     "Último",
+          "sNext":     "Siguiente",
+          "sPrevious": "Anterior"
+        }
+    },
+    className: "btn btn-primary",
+    buttons: [
+        {
+          extend: 'csvHtml5',
+          title: $('#titulo').html(),
+          className: 'btn-success btn-sm'
+        }
+    ]
+  });
+  table.buttons().container().appendTo($('#botonesDatatable'));
+});
